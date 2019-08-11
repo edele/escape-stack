c91f2ca833f83ca343d78724af631c137b9f3bb5
Edele Gizatullin <me@edele.ru>
2019-08-11 14:32:57 +0500

    First version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-function App() {
+import * as React from "react";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion>
+        <Card />
+      </Accordion>
     </div>
   );
 }
 
-export default App;
+function Accordion({ children }) {
+  const [isOpen, setOpen] = React.useState(false);
+
+  useEscape(() => setOpen(false), "accordion");
+
+  return (
+    <div style={{ background: "violet" }}>
+      <button onClick={() => setOpen(x => !x)}>uncollapse or collapse</button>
+      {isOpen && <div>{children}</div>}
+    </div>
+  );
+}
+
+function Card() {
+  const [selected, setSelected] = React.useState(null);
+
+  function onClose() {
+    setSelected(null);
+  }
+
+  const pumps = [
+    { id: "1", name: "Nice pump", imageUrl: "https://i.imgur.com/3GJVznw.jpg" },
+    { id: "2", name: "Nasty pump", imageUrl: "https://i.imgur.com/ifZ6EvP.jpg" }
+  ];
+
+  return (
+    <div>
+      <h3>Uncollapsed accordion content</h3>
+      <ul>
+        {pumps.map(p => (
+          <li key={p.id}>
+            <button onClick={() => setSelected(p.id)}>show {p.name}</button>
+            {selected === p.id && (
+              <ImagePopup url={p.imageUrl} alt={p.name} onClose={onClose} />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+type ImagePopupProps = { url: string, alt: string, onClose: () => void };
+
+function ImagePopup({ url, alt, onClose }: ImagePopupProps) {
+  useEscape(() => onClose(), "popup");
+
+  return (
+    <div className="popup">
+      <img src={url} alt={alt} />
+      <div style={{ color: "violet", fontStyle: "italic" }}>
+        Pumpkin image. Push ESC or{" "}
+        <button onClick={onClose}>click to close image</button>
+      </div>
+    </div>
+  );
+}
+
+// ************** //
+
+// TODO: fix bug if hook is used outside of target
+
+const globalEscapeListenersStack = [];
+const namesStack = [];
+
+document.addEventListener("keyup", keyListener);
+
+function keyListener(event: KeyboardEvent) {
+  if (event.key === "Escape") {
+    const lastHandler =
+      globalEscapeListenersStack[globalEscapeListenersStack.length - 1];
+    lastHandler && lastHandler();
+
+    namesStack.pop();
+  }
+}
+
+function useEscape(onEscape, name) {
+  React.useEffect(() => {
+    globalEscapeListenersStack.push(onEscape);
+
+    return () => {
+      globalEscapeListenersStack.pop();
+    };
+  }, [onEscape]);
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,124 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// @flow
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import * as React from "react";
+import App from "./App";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+test("shows pumps", () => {
+  const { container, clickAccordion } = renderApp();
+
+  clickAccordion();
+
+  expect(container).toHaveTextContent(/uncollapsed accordion content/i);
 });
+
+test("shows and hides pumps via mouse click", () => {
+  const { container, clickAccordion } = renderApp();
+
+  clickAccordion();
+  clickAccordion();
+
+  expect(container).not.toHaveTextContent("Pumpkins");
+});
+
+test("hides pumps via escape key", () => {
+  const { container, clickAccordion, pressEsc } = renderApp();
+
+  clickAccordion();
+  pressEsc();
+
+  expect(container).not.toHaveTextContent("Pumpkins");
+});
+
+test("shows image", () => {
+  const { container, openImage } = renderApp();
+
+  openImage();
+
+  expect(container).toHaveTextContent(/pumpkin image/i);
+});
+
+test("hides image via escape key", () => {
+  const { container, openImage, pressEsc } = renderApp();
+
+  openImage();
+  pressEsc();
+
+  expect(container).not.toHaveTextContent("pumpkin image");
+});
+
+test("accordion is still open when image is closed", () => {
+  const { container, openImage, pressEsc } = renderApp();
+
+  openImage();
+  pressEsc();
+
+  expect(container).toHaveTextContent(/uncollapsed accordion content/i);
+});
+
+test("accordion is hidden when escape pressed twice", () => {
+  const { container, openImage, pressEsc } = renderApp();
+
+  openImage();
+  pressEsc();
+  pressEsc();
+
+  expect(container).not.toHaveTextContent(/uncollapsed accordion content/i);
+});
+
+test("several times accordion is hidden when escape pressed twice", () => {
+  const { container, openImage, pressEsc } = renderApp();
+  const TIMES = 5;
+
+  for (let i = 0; i < TIMES; i++) {
+    openImage();
+    pressEsc();
+    pressEsc();
+
+    expect(container).not.toHaveTextContent(/uncollapsed accordion content/i);
+  }
+});
+
+test("image is hidden via click", () => {
+  const { container, openImage, closeImageViaClick } = renderApp();
+
+  openImage();
+  closeImageViaClick();
+
+  expect(container).not.toHaveTextContent("pumpkin image");
+  expect(container).toHaveTextContent(/uncollapsed accordion content/i);
+});
+
+test("accordion is closed on esc when image was hidden via click", () => {
+  const { container, openImage, pressEsc, closeImageViaClick } = renderApp();
+
+  openImage();
+  closeImageViaClick();
+  pressEsc();
+
+  expect(container).not.toHaveTextContent(/uncollapsed accordion content/i);
+});
+
+function renderApp() {
+  const utils = render(<App />);
+
+  utils.pressEsc = () => {
+    fireEvent.keyUp(utils.container, { key: "Escape" });
+  };
+
+  utils.clickAccordion = () => {
+    fireEvent.click(utils.getByText(/uncollapse or collapse/i));
+  };
+
+  utils.closeImageViaClick = () => {
+    fireEvent.click(utils.getByText(/click to close image/i));
+  };
+
+  utils.openImage = () => {
+    fireEvent.click(utils.getByText(/uncollapse or collapse/));
+    fireEvent.click(utils.getByText(/show nice pump/i));
+  };
+
+  return utils;
+}
